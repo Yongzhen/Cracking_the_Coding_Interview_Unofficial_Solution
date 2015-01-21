78b51b46632d29d09277f925e2bf8d0f42a46a26
Yongzhen <yqiu19@hawk.iit.edu>
2015-01-21 15:11:19 -0600

    add Chapter2

---
+++ Chapter2/src/chapter2/Chapter2.java
@@ -0,0 +1,127 @@
+package chapter2;
+
+public class Chapter2 {
+
+	public static void main(String[] args) {
+		testProblem1();
+		testProblem2();
+		testProblem3();
+		testProblem4();
+		testProblem5();
+	}
+	
+	private static void testProblem1() {
+		System.out.println("\n\nProblem1 test:\n");
+		
+		SingleNode head = new SingleNode(1);
+		head.appendToTail(2);
+		head.appendToTail(3);
+		head.appendToTail(3);
+		head.appendToTail(2);
+		head.appendToTail(4);
+		
+		System.out.println("Before removing duplication");
+		printSingleNodeList(head);
+		
+		SingleNode.removeDuplicate(head);
+		System.out.println("After removing duplication");
+		printSingleNodeList(head);
+		
+		head.appendToTail(4);
+		head.appendToTail(5);
+		head.appendToTail(3);
+		
+		System.out.println("Before removing duplication");
+		printSingleNodeList(head);
+		
+		SingleNode.removeDuplicate2(head);
+		System.out.println("After removing duplication");
+		printSingleNodeList(head);
+	}
+	
+	private static void testProblem2() {
+		System.out.println("\n\nProblem2 test:\n");
+		
+		SingleNode head = new SingleNode(5);
+		head.appendToTail(4);
+		head.appendToTail(3);
+		head.appendToTail(2);
+		head.appendToTail(1);
+		head.appendToTail(0);
+
+		System.out.println("Get 0 to the end: " + SingleNode.findNToTheLast(head, 0).data);
+		System.out.println("Get 3 to the end: " + SingleNode.findNToTheLast(head, 3).data);
+		System.out.println("Get 5 to the end: " + SingleNode.findNToTheLast(head, 5).data);
+	}
+	
+	private static void testProblem3() {
+		System.out.println("\n\nProblem3 test:\n");
+		
+		SingleNode head = new SingleNode(1);
+		head.appendToTail(2);
+		head.appendToTail(3);
+		head.appendToTail(4);
+		head.appendToTail(5);
+		SingleNode middle = head.next.next;
+		
+		System.out.println("Before removing middle");
+		printSingleNodeList(head);
+		
+		SingleNode.deleteNode(middle);
+		System.out.println("After removing middle");
+		printSingleNodeList(head);
+	}
+	
+	private static void testProblem4() {
+		System.out.println("\n\nProblem4 test:\n");
+		
+		SingleNode first = new SingleNode(3);
+		first.appendToTail(9);
+		first.appendToTail(9);
+		
+		SingleNode second = new SingleNode(1);
+		second.appendToTail(2);
+		
+		System.out.print("First Node:");
+		printSingleNodeList(first);
+		System.out.println("PLUS");
+		System.out.print("Second Node:");
+		printSingleNodeList(second);
+		
+		System.out.println("EQUAL:");
+		printSingleNodeList(SingleNode.sumNumber(first, second));
+	}
+	
+	private static void testProblem5() {
+		System.out.println("\n\nProblem4 test:\n");
+		
+		SingleNode head = new SingleNode(1);
+		head.appendToTail(2);
+		head.appendToTail(3);
+		head.appendToTail(4);
+		head.appendToTail(5);
+
+		SingleNode end = head;
+		while(end.next != null) {
+			end = end.next;
+		}
+		end.next = head.next.next;
+		
+		SingleNode loopStart = SingleNode.detectLoop(head);
+		System.out.println("Loop start node's data is:" + loopStart.data);
+	}
+	
+	public static void printSingleNodeList(SingleNode head) {
+		if(head != null) {
+			System.out.print(head.data);
+			SingleNode currentNode = head;
+			while(currentNode.next != null) {
+				currentNode = currentNode.next;
+				System.out.print("--->" + currentNode.data);
+			}
+			System.out.println();
+		}
+	}
+	
+	
+}
---
+++ Chapter2/src/chapter2/SingleNode.java
@@ -0,0 +1,167 @@
+package chapter2;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SingleNode {
+
+	SingleNode next;
+	int data;
+
+	public SingleNode(int d) {
+		data = d; 
+	}
+
+	public void appendToTail(int d) {
+		SingleNode end = new SingleNode(d);
+		SingleNode currentNode = this;
+		while(currentNode.next != null) {
+			currentNode = currentNode.next;
+		}
+		currentNode.next = end;
+	}
+	
+	  /*********************************************/
+	 /***************** Problem 1 *****************/
+	/*********************************************/
+	
+	public static void removeDuplicate(SingleNode head) {
+		Set<Integer> dataSet = new HashSet<Integer>();
+		SingleNode currentNode = head;
+		SingleNode previousNode = null;
+		
+		while(currentNode != null) {
+			if(!dataSet.contains(currentNode.data)) {
+				dataSet.add(currentNode.data);
+				previousNode = currentNode;
+				currentNode = currentNode.next;
+			} else {
+				previousNode.next = currentNode.next;
+				currentNode = currentNode.next;
+			}
+		}
+	}
+	
+	public static void removeDuplicate2(SingleNode head) {
+		SingleNode previousNode = null;
+		SingleNode currentNode = head;
+		while(currentNode != null) {
+			SingleNode iterator = head;
+			while(iterator != currentNode) {
+				if(iterator.data == currentNode.data) {
+					previousNode.next = currentNode.next;
+					currentNode = currentNode.next;
+					break;
+				}
+				iterator = iterator.next;
+			}
+			if(iterator == currentNode) {
+				previousNode = currentNode;
+				currentNode = currentNode.next;
+			}
+		}
+	}
+	
+	  /*********************************************/
+	 /***************** Problem 2 *****************/
+	/*********************************************/
+	
+	public static SingleNode findNToTheLast(SingleNode head, int n) {
+		SingleNode currentNode = head;
+		SingleNode NToCurrent = null;
+		
+		for(int i = 0; i < n; i++) {
+			currentNode = currentNode.next;
+		}
+		if(currentNode == null) {
+			return null; //Number of nodes is less than n
+		}
+		NToCurrent = head;
+		while(currentNode.next != null) { //While current node is not the last one
+			NToCurrent = NToCurrent.next;
+			currentNode = currentNode.next;
+		}
+		return NToCurrent;
+	}
+	
+	  /*********************************************/
+	 /***************** Problem 3 *****************/
+	/*********************************************/
+	
+	//My solution here is not good, it only need to delete that node, but not rotate all after it.
+	public static void rotateForwardOne(SingleNode node) {
+		SingleNode previousNode = null;
+		while(node.next != null) {
+			node.data = node.next.data;
+			previousNode = node;
+			node = node.next;
+		}
+		previousNode.next = null;
+	}
+	
+	//Official solution
+	public static boolean deleteNode(SingleNode node) {
+		if(node == null || node.next == null) {
+			return false;
+		}
+		node.data = node.next.data;
+		node.next = node.next.next;
+		return true;
+	}
+	
+	  /*********************************************/
+	 /***************** Problem 4 *****************/
+	/*********************************************/
+	
+	public static SingleNode sumNumber(SingleNode first, SingleNode second) {
+		SingleNode sumNode = null;
+		int carry = 0;
+		
+		while(first != null || second != null) {
+			int firstFactor = 0;
+			int secondFactor = 0;
+			
+			if(first != null) {
+				firstFactor = first.data;
+				first = first.next;
+			}
+			if(second != null) {
+				secondFactor = second.data;
+				second = second.next;
+			}			
+			int sum = firstFactor + secondFactor + carry;
+			carry = sum / 10;
+			
+			if(sumNode == null) {
+				sumNode = new SingleNode(sum % 10);
+			} else {
+				sumNode.appendToTail(sum % 10);
+			}
+		}
+		if(carry > 0) {
+			sumNode.appendToTail(carry);
+		}
+		return sumNode;
+	}
+	
+	  /*********************************************/
+	 /***************** Problem 5 *****************/
+	/*********************************************/
+	
+	//This solution used HashSet to simplify the problem, instead if we can't use any buffer, it will need to do the trick like the official solution, keep two pointers moving at different speed.
+	public static SingleNode detectLoop(SingleNode head) {
+		Set<SingleNode> nodeSet = new HashSet<SingleNode>();
+		SingleNode currentNode = head;
+		
+		while(currentNode != null) {
+			if(!nodeSet.contains(currentNode)) {
+				nodeSet.add(currentNode);
+				currentNode = currentNode.next;
+			} else {
+				break;
+			}
+		}
+		return currentNode;
+	}
+	
+}
